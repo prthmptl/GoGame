fa8135a3a6d4c56ca448852e3dbc4f3f4bb9db99
Pratham Patel <pratham.patel@prakashinfotech.com>
2026-05-06 10:46:25 +0530

    Updated target API level to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.weiqi"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.weiqi"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 3
         versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
